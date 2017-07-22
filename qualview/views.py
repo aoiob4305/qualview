0046a9079c67235262aeae132dc784d4b4eee3cb
aoiob4305 <aoiob4305@gmail.com>
2017-07-23 07:49:51 +0900

    20170723

--- qualview/views.py
+++ qualview/views.py
@@ -10,4 +10,5 @@ def mainview(request):
     return render(request, 'qualview/main.html', {'lists': lists, 'amounts': amounts, 'today': today})
 
 def monthlyview(request):
-    return render(request, 'qualview/monthly.html', {})
+    today = datetime.now().strftime('%Y-%m-%d')
+    return render(request, 'qualview/monthly.html', {'today': today})
